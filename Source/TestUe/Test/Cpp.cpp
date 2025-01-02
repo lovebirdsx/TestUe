2e47749389951de44a7587de91fbfb464dc50901
lovebird <lovebirdsx@gmail.com>
2025-01-02 18:28:50 +0800

    加入cpp相关测试

---
+++ Source/TestUe/Test/Cpp.cpp
@@ -0,0 +1,54 @@
+﻿#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(Cpp_Constructor, "TestUe.Cpp.Constructor", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+int32 G_BaseConstructCallCount = 0;
+int32 G_BaseDestructCallCount = 0;
+int G_DerivedConstructCallCount = 0;
+int G_DerivedDestructCallCount = 0;
+
+class FBase
+{
+public:		
+	FBase()
+	{
+		G_BaseConstructCallCount++;			
+	}
+
+	virtual ~FBase()
+	{
+		G_BaseDestructCallCount++;			
+	}
+};
+
+class FDerived : public FBase
+{
+public:
+	FDerived()
+	{
+		G_DerivedConstructCallCount++;
+	}
+
+	virtual ~FDerived() override
+	{
+		G_DerivedDestructCallCount++;
+	}
+};
+
+bool Cpp_Constructor::RunTest(const FString& Parameters)
+{
+	G_BaseConstructCallCount = 0;
+	G_BaseDestructCallCount = 0;
+	G_DerivedConstructCallCount = 0;
+	G_DerivedDestructCallCount = 0;
+	
+	const FBase *Base = new FDerived();
+	delete Base;
+
+	TestEqual("G_BaseConstructCallCount must be 1", G_BaseConstructCallCount, 1);
+	TestEqual("G_BaseDestructCallCount must be 1", G_BaseDestructCallCount, 1);
+	TestEqual("G_DerivedConstructCallCount must be 1", G_DerivedConstructCallCount, 1);
+	TestEqual("G_DerivedDestructCallCount must be 1", G_DerivedDestructCallCount, 1);
+	
+	return true;
+}
